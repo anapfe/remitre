07945c6b949d3b9e8f94cfa2061ba6b79050bde8
Ana Pfefferkorn <anapfefferkorn@gmail.com>
2019-01-16 09:35:09 -0300

    meta name description

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -5,6 +5,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Repuestos electrodomésticos"
 
   <link rel="icon" href="{{asset('favicon.ico')}}" type="image/x-icon"/>
   <link rel="shortcut icon" href="{{asset('favicon.ico')}}" type="image/x-icon"/>
