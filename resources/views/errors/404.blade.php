d439f0c2051b016fe81371e1e185ad3a5ad6f13f
Ana Pfefferkorn <anapfefferkorn@gmail.com>
2019-03-11 16:21:11 -0300

    Update 404.blade.php

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,3 +1,11 @@
+@extends('layouts.layout')
 
+@section('content')
   <h2>{{ $exception->getMessage() }}</h2>
-  <h2>lala error</h2>
+  <h3>No se encontró la página que estabas buscando</h3>
+  <a href="/">Volver a inicio</a>
+@endsection
+
+@section('js')
+  <script src="{{ asset('js/home.js') }}"></script>
+@endsection
